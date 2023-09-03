10409a002af3b79d3033df591baf180ae2457b2a
Aliaa Nasser <125163801+AliaaNasser7@users.noreply.github.com>
2023-09-03 06:49:30 -0700

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,19 @@
+let output=document.getElementById("calc")
+
+function display(num){
+    output.value+=num;
+}
+function Clear(){
+    output.value="";
+}
+function Delete(){
+    output.value=output.value.slice(0,-1);
+}
+function calc(){
+    try{
+       output.value= eval(output.value);
+    }
+    catch(error){
+        output.value="Error";
+    }
+}
